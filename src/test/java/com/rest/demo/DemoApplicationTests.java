fde09cc00406adbc1d24fc9c926679355f5805f4
Cengiz Selvi <cngzslv42@gmail.com>
2023-07-30 14:18:48 +0300

    Initial commit

---
+++ src/test/java/com/rest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
