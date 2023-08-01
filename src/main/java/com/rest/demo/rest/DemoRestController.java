a0ffa7bf43fd582f09727f6443e9142b00e2a3e5
Cengiz Selvi <cngzslv42@gmail.com>
2023-08-01 16:20:59 +0300

    Update

--- src/main/java/com/rest/demo/rest/DemoRestController.java
+++ src/main/java/com/rest/demo/rest/DemoRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")cx
+@RequestMapping("/test")
 
 public class DemoRestController {
 
